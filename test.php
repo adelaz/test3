5143b9b3dfd48aa739d45e83958ea6f574ae2fd1
Azimut Training Center <office@azimutvision.ro>
2017-07-13 12:58:16 +0300

    var4

--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "Invat GIT. Prima modificare.";
- 
- ?>
--- test2.php
+++ test2.php
@@ -1,5 +1,5 @@
 <?php
 
 echo "Invat GIT - Test 2 - modific acest fisier. Il modific din nou.";
- 
+ echo "Informatie noua";
  ?>
