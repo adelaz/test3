418daa3465678af1fb2580cce4e7810a04fb8b8d
Azimut Training Center <office@azimutvision.ro>
2017-07-17 11:01:38 +0300

    r5v1

--- test2.php
+++ test2.php
@@ -3,4 +3,6 @@
 echo "Invat GIT - Test 2 - modific acest fisier. Il modific din nou.";
 echo "Informatie noua";
 echo "Alta modificare";
+Echo "Nu am trimis modificarea aceasta";
+echo "Modificare pe ramura 5";
  ?>
